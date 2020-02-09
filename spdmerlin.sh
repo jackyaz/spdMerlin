489115ed4a7b94fd2ea820742eb5c3041d1fde4b
Jack <jack.naisbett@gmail.com>
2020-02-09 11:41:40 +0000

    Add icon

--- spdmerlin.sh
+++ spdmerlin.sh
@@ -143,6 +143,7 @@ Update_Version(){
 		Update_File "$ARCH.tar.gz"
 		Update_File "spdstats_www.asp"
 		Update_File "redirect.htm"
+		Update_File "addons.png"
 		Update_File "chart.js"
 		Update_File "chartjs-plugin-zoom.js"
 		Update_File "chartjs-plugin-annotation.js"
@@ -168,6 +169,7 @@ Update_Version(){
 			Update_File "$ARCH.tar.gz"
 			Update_File "spdstats_www.asp"
 			Update_File "redirect.htm"
+			Update_File "addons.png"
 			Update_File "chart.js"
 			Update_File "chartjs-plugin-zoom.js"
 			Update_File "chartjs-plugin-annotation.js"
@@ -209,7 +211,7 @@ Update_File(){
 			Mount_WebUI
 		fi
 		rm -f "$tmpfile"
-	elif [ "$1" = "chart.js" ] || [ "$1" = "chartjs-plugin-zoom.js" ] || [ "$1" = "chartjs-plugin-annotation.js" ] || [ "$1" = "moment.js" ] || [ "$1" =  "hammerjs.js" ] || [ "$1" = "chartjs-plugin-datasource.js" ] || [ "$1" = "redirect.htm" ]; then
+	elif [ "$1" = "chart.js" ] || [ "$1" = "chartjs-plugin-zoom.js" ] || [ "$1" = "chartjs-plugin-annotation.js" ] || [ "$1" = "moment.js" ] || [ "$1" =  "hammerjs.js" ] || [ "$1" = "chartjs-plugin-datasource.js" ] || [ "$1" = "redirect.htm" ] || [ "$1" = "addons.png" ]; then
 		tmpfile="/tmp/$1"
 		Download_File "$SHARED_REPO/$1" "$tmpfile"
 		if [ ! -f "$SHARED_DIR/$1" ]; then
@@ -414,6 +416,7 @@ Create_Symlinks(){
 	ln -s "$SHARED_DIR/hammerjs.js" "$SHARED_WEB_DIR/hammerjs.js" 2>/dev/null
 	ln -s "$SHARED_DIR/moment.js" "$SHARED_WEB_DIR/moment.js" 2>/dev/null
 	ln -s "$SHARED_DIR/redirect.htm" "$SHARED_WEB_DIR/redirect.htm" 2>/dev/null
+	ln -s "$SHARED_DIR/addons.png" "$SHARED_WEB_DIR/addons.png.htm" 2>/dev/null
 }
 
 Conf_Exists(){
@@ -682,7 +685,7 @@ Mount_WebUI(){
 		fi
 		
 		if ! grep -q '.menu_Addons' /tmp/index_style.css ; then
-			echo ".menu_Addons{ background: url(ext/shared-jy/addons.png); }" >> /tmp/index_style.css
+			echo ".menu_Addons { background: url(ext/shared-jy/addons.png); }" >> /tmp/index_style.css
 		fi
 		
 		umount /www/index_style.css 2>/dev/null
@@ -1445,6 +1448,7 @@ Menu_Install(){
 	
 	Update_File "spdstats_www.asp"
 	Update_File "redirect.htm"
+	Update_File "addons.png"
 	Update_File "chart.js"
 	Update_File "chartjs-plugin-zoom.js"
 	Update_File "chartjs-plugin-annotation.js"
