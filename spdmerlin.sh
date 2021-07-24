27c8a9e0d3ed3d8214f1e9ada8101cc41c2093b2
Jack Naisbett <jack.naisbett@gmail.com>
2021-07-24 19:05:48 +0100

    Version bump

--- spdmerlin.sh
+++ spdmerlin.sh
@@ -30,8 +30,8 @@
 ### Start of script variables ###
 readonly SCRIPT_NAME="spdMerlin"
 readonly SCRIPT_NAME_LOWER=$(echo $SCRIPT_NAME | tr 'A-Z' 'a-z')
-readonly SCRIPT_VERSION="v4.3.0"
-SCRIPT_BRANCH="master"
+readonly SCRIPT_VERSION="v4.3.1"
+SCRIPT_BRANCH="develop"
 SCRIPT_REPO="https://raw.githubusercontent.com/jackyaz/$SCRIPT_NAME/$SCRIPT_BRANCH"
 readonly SCRIPT_DIR="/jffs/addons/$SCRIPT_NAME_LOWER.d"
 readonly SCRIPT_WEBPAGE_DIR="$(readlink /www/user)"
